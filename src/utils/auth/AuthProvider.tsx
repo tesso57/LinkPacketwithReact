1cb94b88bc13494a5f3e0505f77e5a51f2dd1e7f
tesso <ta4780@icloud.com>
2021-01-12 02:39:48 +0900

    AuthProvider

--- src/utils/auth/AuthProvider.tsx
+++ src/utils/auth/AuthProvider.tsx
@@ -1,1 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
+import { auth } from "../../firebase"
+
+type Context{
+
+}
+
+const AuthContext = React.createContext<Context>('')
+
+const AuthProvider: React.FC = ({children}) => {
+
+    return(
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export {AuthContext, AuthProvider}
