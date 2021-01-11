08219f3c5a5b0a628ec3921269dfa341201ae261
tesso <ta4780@icloud.com>
2021-01-12 02:32:50 +0900

    app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import styles from './App.module.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    </>
+  )
 }
 
 export default App;
