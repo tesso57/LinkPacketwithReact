076ce73609c99291546bb16ea0d49d758c42d9a7
Sabaniki <sawada13@mccc.jp>
2021-01-13 04:53:05 +0900

    とりあえずそれっぽく表示できました。

--- src/pages/PacketDetails.tsx
+++ src/pages/PacketDetails.tsx
@@ -1,19 +1,19 @@
 import React, {useEffect, useState} from 'react'
 import {RouteComponentProps} from "react-router";
 import {db, firebase} from '../firebase';
-import {Packet, Tag, User} from "../utils/types";
+import {Packet, User} from "../utils/types";
+import {Button, Container, Icon, List, ListItem, ListItemText, Paper} from "@material-ui/core";
+import {inspect} from "util";
+import styles from './PacketDetails.module.scss';
 
 type UrlProps = {} & RouteComponentProps<{ packetId: string }>
 
 const PacketDetails: React.FC<UrlProps> = (props) => {
     const [packet, setPacket] = useState<Packet | undefined>(undefined);
     useEffect(() => {
-        console.log('rendering...');
-//        if (props.match.params.uid === undefined) return;
-        console.log('defined' + props.match.params.packetId);
-        const docRef = db.collection('packets').doc('QNTSF87b1D3HEfMwhgHk');
-        const doc = docRef.get().then(doc => {
-            console.log('in doc');
+        if (props.match.params.packetId === undefined) return;
+        const docRef = db.collection('packets').doc(props.match.params.packetId);
+        docRef.get().then(doc => {
             if (doc.exists) {
                 console.log(doc.data());
                 setPacket(doc.data() as Packet)
@@ -22,10 +22,28 @@ const PacketDetails: React.FC<UrlProps> = (props) => {
             }
         })
     }, [props.match.params.packetId]);
+
+    const onClickItem = (link: string) => () => window.location.replace(link);
+
     return (<>
-        <div>
-            {packet?.title}
-        </div>
+        <Container maxWidth={"md"}>
+            <h3>
+                {packet?.title}
+            </h3>
+            <List component={"nav"}>
+                {
+                    packet?.urls.map((url, i) => (
+                        <Paper elevation={2}>
+                            <ListItem key={i} className={styles.list} button>
+                                <ListItemText primary={url.title} secondary={url.link}
+                                              onClick={onClickItem(url.link)}/>
+                            </ListItem>
+                        </Paper>
+                    ))
+                }
+            </List>
+            {/*<Button variant={"contained"}>hoge</Button>*/}
+        </Container>
     </>)
 };
 
--- src/utils/types/url.d.ts
+++ src/utils/types/url.d.ts
@@ -1,5 +1,4 @@
 export type URL = {
-    id: string;
     link: string;
     title: string;
 }
