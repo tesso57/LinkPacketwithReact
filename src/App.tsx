41967e36bce055e94bdb3084cdaa1629387370dc
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-12 22:16:40 +0900

    サインイン周りのheaderの変化がいい感じに

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { AuthProvider } from './utils/auth/AuthProvider';
+import { Header } from './components/Layout/Header';
 
 const App: React.FC = () => {
   return (
-
-    <>
-    </>
+    <AuthProvider>
+      <Header />
+    </AuthProvider>
   )
 }
 
