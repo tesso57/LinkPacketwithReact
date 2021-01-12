3a729df3e84d19c706536bd311111a573f50fda3
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-13 02:12:16 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -7,35 +7,12 @@ import User from './pages/Users';
 import Packet from './pages/Packet';
 import Create from './pages/Create';
 import View from './pages/View';
-import BookmarkCardList from './components/BookmarkCardList';
-
-interface Card {
-  title: string,
-  description: string,
-  url: string;
-}
-
-const cards: Card[]  = [
-  {title: 'test1', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test2', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test3', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-  {title: 'test4', description : 'this is description', url : 'https://qiita.com/ksyunnnn/items/bfe2b9c568e97bb6b494'},
-];
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <Router>
         <Header />
-        <BookmarkCardList cards={cards} />
         <Switch>
           <Route path={'/users/:userId'} component={User} />
           <Route path={'/packet/:packetId'} component={Packet} />
