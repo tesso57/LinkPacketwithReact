bbb591b75b2fb0dd2809f00d41ea85321bbf86c9
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-14 01:17:00 +0900

    修正

--- src/utils/types/user.d.ts
+++ src/utils/types/user.d.ts
@@ -3,7 +3,7 @@ import { firebase } from "../../firebase"
 export type User = {
     id: string;
     packetRefs: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>[];
-    // subscribePacketRefs: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>[];
+    subscribePacketRefs: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>[];
     displayName: string | null;
     photoUrl: string | null;
 }
