b646d998e31f9d387f041945f0ee280064f3686c
tesso <ta4780@icloud.com>
2021-01-14 02:01:16 +0900

    少し変更

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import PageContainer from '../components/Layout/PageContainer'
 
 
 const Edit : React.FC = () => {
-    return(<></>)
+    return(
+        <PageContainer>
+
+        </PageContainer>
+    )
 }
 
 export default Edit;
