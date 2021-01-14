aeaf56f9d17bdfeb37e099bafe7fbc933c17e7d4
tesso <ta4780@icloud.com>
2021-01-15 01:41:37 +0900

    マージンを追加

--- src/pages/PacketDetails.tsx
+++ src/pages/PacketDetails.tsx
@@ -5,6 +5,7 @@ import {Packet} from "../utils/types";
 import { Container, List, ListItem, ListItemText, Paper} from "@material-ui/core";
 import styles from './PacketDetails.module.scss';
 import {useHistory} from "react-router-dom";
+import PageContainer from "../components/Layout/PageContainer"
 
 type UrlProps = {} & RouteComponentProps<{ packetId: string }>
 
@@ -25,26 +26,29 @@ const PacketDetails: React.FC<UrlProps> = (props) => {
         })
     }, [props.match.params.packetId, history]);
 
-    return (<>
-        <Container maxWidth={"md"}>
-            <h3>
-                {packet?.title}
-            </h3>
-            <List component={"nav"}>
-                {
-                    packet?.urls.map((url, i) => (
-                        <Paper elevation={2}>
-                            <ListItem key={i} className={styles.list} button>
-                                <ListItemText primary={url.title} secondary={url.link}
-                                              onClick={onClickItem(url.link)}/>
-                            </ListItem>
-                        </Paper>
-                    ))
-                }
-            </List>
-            {/*<Button variant={"contained"}>hoge</Button>*/}
-        </Container>
-    </>)
+    return (
+        <PageContainer>
+            <Container maxWidth={"md"}>
+                <h3>
+                    {packet?.title}
+                </h3>
+                <List component={"nav"} className={styles.listContainer}>
+                    {
+                        packet !== undefined && 
+
+                        packet.urls.map((url, i) => (
+                            <Paper elevation={2}>
+                                <ListItem key={i} className={styles.list} button>
+                                    <ListItemText primary={url.title} secondary={url.link}
+                                                onClick={onClickItem(url.link)}/>
+                                </ListItem>
+                            </Paper>
+                        ))
+                    }
+                </List>
+            </Container>
+        </PageContainer>
+    )
 };
 
 export default PacketDetails;
