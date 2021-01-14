5b718649425fbb9f192d193c8e52c3d70c3da797
tesso <ta4780@icloud.com>
2021-01-15 00:46:03 +0900

    create ボタンを調整

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -129,11 +129,15 @@ const Users : React.FC<urlProps> = (props) => {
                     </>
                 }
             </div>
-            <div className={styles.createButton}>
-                <IconButton onClick={createButton}>
-                    <AddIcon style={{ fontSize: 100,color:`#fff`,backgroundColor:`#F6B40D`,borderRadius:`50%`}} />
-                </IconButton>
-            </div>
+            {
+                 (currentUser !== null &&  history.location.pathname.includes(`users/${currentUser.id}`)) && 
+                 <div className={styles.createButton}>
+                 <IconButton onClick={createButton}>
+                     <AddIcon style={{ fontSize: 100,color:`#fff`,backgroundColor:`#F6B40D`,borderRadius:`50%`}} />
+                 </IconButton>
+             </div>
+            }
+
         </PageContainer>
     )
 }
