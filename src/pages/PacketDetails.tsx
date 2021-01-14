b98c107426422d12c2697290de6f5e8a88381383
tesso <ta4780@icloud.com>
2021-01-15 05:15:47 +0900

    出来た

--- src/pages/PacketDetails.tsx
+++ src/pages/PacketDetails.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState, useCallback} from 'react'
+import React, {useEffect, useState} from 'react'
 import {RouteComponentProps} from "react-router";
 import {db} from '../firebase';
-import {Packet} from "../utils/types";
+import {Packet,URL} from "../utils/types";
 import {Container, IconButton, List, ListItem, ListItemText, Paper, Tooltip} from "@material-ui/core";
 import styles from './PacketDetails.module.scss';
 import {useHistory} from "react-router-dom";
@@ -9,52 +9,73 @@ import AssignmentIcon from '@material-ui/icons/Assignment';
 import CopyToClipBoard from 'react-copy-to-clipboard';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
+
+type Props = {
+    url : URL;
+    key : number;
+}
+
+const Bookmark :React.FC<Props> = (props) => {
+    const [open, setOpen] = useState<boolean>(false)
+    return (
+        <Paper elevation={2}>
+            <ListItem key={props.key} className={styles.list} button>
+                <ListItemText primary={props.url.title} secondary={props.url.link}
+                                onClick={onClickItem(props.url.link)}/>
+            </ListItem>
+                <ClickAwayListener onClickAway={() => setOpen(false)}>
+                    <div>
+                        <Tooltip
+                            PopperProps={{
+                                disablePortal: true,
+                        }}
+                            arrow
+                            open={open}
+                            onClose={() => setOpen(false)}
+                            placement='top'
+                            disableFocusListener
+                            disableHoverListener
+                            disableTouchListener
+                            title='URL Copied!'
+                        >
+                            <CopyToClipBoard text={props.url.link}>
+                                <IconButton
+                                    onClick={ () => setOpen(true)}
+                                >
+                                    <AssignmentIcon/>
+                                </IconButton>
+                            </CopyToClipBoard>
+                        </Tooltip>
+                    </div>
+                </ClickAwayListener>
+        </Paper>
+    )
+}
+
+
+
 type UrlProps = {} & RouteComponentProps<{ packetId: string }>
 
 const onClickItem = (link: string) => () => window.location.replace(link);
 
 
+
 const PacketDetails: React.FC<UrlProps> = (props) => {
     const [packet, setPacket] = useState<Packet | undefined>(undefined);
     const history = useHistory();
-    const [openTips, setOpenTips] = useState<boolean[] | undefined>(undefined);
-
-    const handleCloseTip = useCallback((i: number) => {
-        setOpenTips(currentTips => {
-            if (currentTips === undefined) return;
-            const tmpOpenTips = currentTips;
-            tmpOpenTips[i] = false;
-            return tmpOpenTips;
-        })
-    },[]);
-
-    const handleOpenTip = useCallback((i: number) => {
-        setOpenTips(currentTips => {
-            if (currentTips === undefined) return;
-            const tmpOpenTips = currentTips;
-            tmpOpenTips[i] = true;
-            return tmpOpenTips;
-        })
-    },[]);
 
     useEffect(() => {
         if (props.match.params.packetId === undefined) return;
         const docRef = db.collection('packets').doc(props.match.params.packetId);
-        console.log("get packet")
         docRef.get().then(doc => {
-            const data = doc.data() as Packet
-            if (doc.exists && data !== undefined) {
-                setPacket(data)
-                const tmpOpenTips = new Array<boolean>();
-                if(data?.urls !== undefined)
-                    data.urls.forEach(_ => tmpOpenTips.push(false));
-                setOpenTips(tmpOpenTips)
+            if (doc.exists) {
+                setPacket(doc.data() as Packet)
             } else {
                 history.push('/')
             }
         })
     }, [props.match.params.packetId, history]);
-    console.log("render")
+
     return (<>
         <Container maxWidth={"md"}>
             <h3>
@@ -63,41 +84,7 @@ const PacketDetails: React.FC<UrlProps> = (props) => {
             <List component={"nav"}>
                 {
                     packet?.urls.map((url, i) => (
-                        <Paper elevation={2}>
-                            <ListItem key={i} className={styles.list} button>
-                                <ListItemText primary={url.title} secondary={url.link}
-                                              onClick={onClickItem(url.link)}/>
-                            </ListItem>
-                            {
-                                openTips !== undefined &&
-                                <ClickAwayListener onClickAway={() => handleCloseTip(i)}>
-                                    <div>
-                                        <Tooltip
-                                            PopperProps={{
-                                                disablePortal: true,
-                                        }}
-                                            arrow
-                                            open={openTips[i]}
-                                            placement='top'
-                                            onClose={() => handleCloseTip(i)}
-                                            disableFocusListener
-                                            disableHoverListener
-                                            disableTouchListener
-                                            title='URL Copied!'
-                                        >
-                                            {/* <CopyToClipBoard text={url.link}> */}
-                                                <IconButton
-                                                    onClick={ () => handleOpenTip(i)}
-                                                >
-                                                    <AssignmentIcon/>
-                                                </IconButton>
-                                            {/* </CopyToClipBoard> */}
-                                        </Tooltip>
-                                    </div>
-                                </ClickAwayListener>
-                            }
-                            
-                        </Paper>
+                        <Bookmark url={url} key={i}/>
                     ))
                 }
             </List>
