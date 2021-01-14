98b008f9b66ad8f9e34cf775a1ab1c55618110eb
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-15 00:52:15 +0900

    追加ブックマークを編集しやすく

--- src/components/EditBookmark.tsx
+++ src/components/EditBookmark.tsx
@@ -1,24 +1,32 @@
 import React, { FC } from 'react';
 import { Paper, ListItem, ListItemText, TextField, IconButton, Tooltip } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
+import { withStyles } from '@material-ui/core/styles';
 import { URL } from '../utils/types/url';
 
 type Props = {
-    url?: URL,
-    changeTitle: (newTitle: string) => void,
-    changeUrl: (newUrl: string) => void,
-    add: () => void
+  url?: URL,
+  changeTitle: (newTitle: string) => void,
+  changeUrl: (newUrl: string) => void,
+  add: () => void
 };
+
+const StyledTextField = withStyles({
+  root: {
+    width: "60%",
+    margin: "2rem 0",
+  }
+})(TextField);
 
 const EditBookmark: FC<Props> = (props) => (
   <Paper elevation={4}>
     <ListItem>
       <ListItemText
         primary={
-          <TextField type="text" placeholder="Bookmark title" defaultValue={props.url?.title} onChange={(e) => props.changeTitle(e.target.value)} size="medium" />
+          <StyledTextField type="text" placeholder="Bookmark title" defaultValue={props.url?.title} onChange={(e) => props.changeTitle(e.target.value)} size="medium" />
         }
         secondary={
-          <TextField type="text" placeholder="Bookmark URL" defaultValue={props.url?.link} onChange={(e) => props.changeUrl(e.target.value)} size="medium" />
+          <StyledTextField type="text" placeholder="Bookmark URL" defaultValue={props.url?.link} onChange={(e) => props.changeUrl(e.target.value)} size="medium" />
         }
       />
       <Tooltip title="Add">
