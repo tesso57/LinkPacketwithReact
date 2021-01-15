4866b7bbeb7b012ca390fe225b795570d9797904
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-15 21:57:32 +0900

    footerを追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import { AuthProvider } from './utils/auth/AuthProvider';
 import Header from "./components/Layout/Header";
+import Footer from './components/Layout/Footer';
 import User from './pages/Users';
 import EditPacket from './pages/EditPacket';
 import PacketDetails from './pages/PacketDetails';
@@ -15,11 +16,11 @@ const App: React.FC = () => {
         <Switch>
           <Route exact path={'/edit/:packetId'} component={EditPacket} />
           <Route exact path={'/users/:userId'} component={User} />
-          <Route exact path={'/users/:userId'} component={User} />
           <Route exact path={'/packet/:packetId'} component={PacketDetails} />
           <Route exact path={'/'} component={View} />
           <Redirect to={''}/>
         </Switch>
+        <Footer />
       </Router>
     </AuthProvider>
   )
