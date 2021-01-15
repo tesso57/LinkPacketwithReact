60d50b34cbcd66836cf52edf96fee1775582f57d
rytaryu <53173674+rytar@users.noreply.github.com>
2021-01-15 22:32:34 +0900

    パケットギャラリーみたいに縦に長くて画面からはみ出すような要素は下にちょっとmargin設定しておいた方がいしておいた方がいいかも

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -4,12 +4,12 @@ import styles from './Footer.module.scss';
 import faviconURL from '../../favicon.png';
 
 const Footer: FC = () => (
-    <Toolbar className={styles.Footer}>
-        <div className={styles.FooterElements}>
-            <img className={styles.FooterImage} src={faviconURL} alt="favicon"></img>
-            <p className={styles.FooterText}>© 2020LinkPacket</p>
-        </div>
-    </Toolbar>
+  <Toolbar className={styles.Footer}>
+    <div className={styles.FooterElements}>
+      <img className={styles.FooterImage} src={faviconURL} alt="favicon"></img>
+      <p className={styles.FooterText}>© 2020LinkPacket</p>
+    </div>
+  </Toolbar>
 );
 
 export default Footer;
