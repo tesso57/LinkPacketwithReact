0236f8cbeb3adbd93ce0fc80b0b76f6d1bc4770e
Sabaniki <sawada13@mccc.jp>
2021-01-16 15:49:11 +0900

    わからん。行った可能性もそうでない可能性も同時に存在する。

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -11,4 +11,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
 reportWebVitals();
+serviceWorker.unregister();
